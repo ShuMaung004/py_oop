9bebde34b1a746c5f8a084299686d9d900a69363
Shu Maung <shumaung004@gmail.com>
2025-08-24 12:26:25 +0700

    add venv

---
+++ test_env.py
@@ -0,0 +1,6 @@
+from colorama import Fore,Back, Style, init
+
+init()
+print(Fore.RED + "This is red TExt")
+print(Fore.GREEN + "this is green text")
+print(Back.YELLOW + "this has yellow background")
